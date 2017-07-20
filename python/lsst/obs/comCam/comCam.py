70c0c859576bea959487de56dd6e14d781a1bf3f
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2017-07-20 17:26:16 -0500

    Move yaml camera to obs_base

--- python/lsst/obs/comCam/comCam.py
+++ python/lsst/obs/comCam/comCam.py
@@ -21,7 +21,7 @@
 #
 import os.path
 import lsst.utils as utils
-from yamlCamera import YamlCamera
+from lsst.obs.base.yamlCamera import YamlCamera
 
 class ComCam(YamlCamera):
     """The Commissioning Camera (comCam)
