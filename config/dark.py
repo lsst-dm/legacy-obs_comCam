cb0fd33b51dcfae9748d7d0e73e7a9cb22928170
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2018-06-19 15:51:47 -0500

    Update obs_comCam to work with brighter-fatter processing

--- config/dark.py
+++ config/dark.py
@@ -1,3 +1,4 @@
 config.isr.doLinearize = False
 config.isr.doDefect = False
+config.isr.doAddDistortionModel = False
 config.repair.cosmicray.nCrPixelMax = 100000
--- config/flat.py
+++ config/flat.py
@@ -1,2 +1,3 @@
 config.isr.doLinearize = False
 config.isr.doDefect = False
+config.isr.doAddDistortionModel = False
